21ccd31f2c6c5a6850e28eb2cf6ce56b9c653746
Kovacs Ervin <kovacs.ervin@sonrisa.hu>
2023-03-16 19:52:42 +0200

    subcommands

--- pytris.py
+++ pytris.py
@@ -1,67 +1,57 @@
 from typing import List, TypeVar, Generic, Callable
+from abc import ABC, abstractmethod
 import random
 import time
-from enum import Enum
 import os
 import sys
 import re
+from enum import Enum
 
 T = TypeVar('T')
 
-# TYPES START
+
+class Subcommand(ABC):
+    @abstractmethod
+    def validate():
+        pass
+
+
 class Position:
     def __init__(self, row_index: int, col_index: int) -> None:
         self.row_index = row_index
         self.col_index = col_index
 
 
-class GridSize:
-    def __init__(self, rows, columns) -> None:
-        self.rows = rows
-        self.columns = columns
+class GridSize(Subcommand):
+    def __init__(self) -> None:
+        self.name = 'grid_size'
+        self.shorthand = 'GS'
+
+    def validate(self, arg_value: str):
+        allowed_format = "<width: int><separator: @see allowed_separators><height: int>"
+        pattern = r'^(?=[1-9][0-9]{1})(?=[^,x]{0,2}[x,][^,x]{0,2}|[^,x]{0,2}[,][^,x]{0,2})[1-9][0-9]{1}[,x][1-9][0-9]{1}$'
+        if re.match(pattern, arg_value):
+            values = arg_value.split(arg_value[2])
+            return (values[0], values[1]), None
+        else:
+            return None, f"Invalid value for argument: {self.name}, expected format: {allowed_format} actual value: {arg_value}"
 
 
-class GameSettings:
-    #                  TODO: 
-#                      maybe this could be some kind of a structure with the setting and its type 
-#                      the setting type itself may be able to parse itself?
-#                      ^
-    def __init__(self, grid_size: GridSize) -> None:
-        self.grid_size = grid_size
-# TYPES END
+class SubcommandParser:
+    def __init__(self, settings: List[Subcommand]) -> None:
+        self.settings = settings
 
-
-# ARGUMENT PARSER START
-class Subcommand(Generic[T]):
-    # The configuration for a single argument:
-    # an arg should look like:
-    # -- with fully specified name: --<name> <value>
-    # -- with shorthand specified: -<shorthand <value>
-    # eg: --grid-size 50,10 -> where '--grid-size' is the name and '50,10' is the value
-    # eg: -GS 50,10         -> where '-GS' is the shorthand and '50,10' is the value
-    def __init__(self, name: str, shorthand: str, value_validator: Callable[[str, str], (tuple[T, None] | tuple[None, str])], is_mandatory: bool) -> None:
-        self.name = name
-        self.shorthand = shorthand
-        self.value_validator = value_validator
-
-
-class ArgumentParserConfig:
-    def __init__(self, subcommands: List[Subcommand]) -> None:
-        self.subcommands = subcommands
-
-
-class ArgumentParser:
-    def __init__(self, config: ArgumentParserConfig) -> None:
-        self.available_arguments = config.subcommands
-
-    def parse(self, argument_string: List[str]) -> GameSettings:
+    def parse(self, argv_str) -> GameSettings:
         valid_results = []
-        pairs = self.parse_to_pairs(argument_string)
+        pairs = self.parse_to_pairs(argv_str)
+
         pair_by_validator = self.get_pairs_by_validator(pairs)
+
         for entry in pair_by_validator:
             print(entry)
             if entry[0] is not None:
-                validation = entry[0].value_validator(entry[0].name, entry[1][1])
+                validation = entry[0].value_validator(
+                    entry[0].name, entry[1][1])
                 match validation:
                     case (_, None):
                         valid_results.append(validation)
@@ -77,49 +67,32 @@ class ArgumentParser:
         return argument_pairs
 
     def get_pairs_by_validator(self, pairs: List[tuple[str, None] | tuple[str, str]]):
-        find_validator_result = []
+        result = []
         for idx, pair in enumerate(pairs):
-            t = self.find_validator_by_name(pair, idx)
-            find_validator_result.append((t[0], pair))
-        return find_validator_result
+            t = self.find_setting(pair, idx)
+            result.append((t[0], pair))
+        return result
 
-    def find_validator_by_name(self, pair: (tuple[str, None] | tuple[str, str]), index: int) -> (tuple[Subcommand, None] | tuple[None, str]):
+    def find_setting(self, pair: (tuple[str, None] | tuple[str, str]), index: int) -> (tuple[Subcommand, None] | tuple[None, str]):
         if not pair[0].startswith("--"):
             return None, f"Error parsing argument [{index}] [{pair[0]}]: Argument name must start with: '--'"
 
         argument_name = pair[0][2:]
-        for arg in self.available_arguments:
-            if arg.name == argument_name:
-                return arg, None
+        for s in self.settings:
+            if s.name == argument_name:
+                return s, None
         return None, f"Invalid argument [{index}] [{pair[0]}]"
 # ARGUMENT PARSER END
 
 
 # Arguments:
 # grid_size
-def grid_size_validator(argument_name: str, value: str):
-    allowed_format = "<width: int><separator: @see allowed_separators><height: int>"
-    pattern = r'^(?=[1-9][0-9]{1})(?=[^,x]{0,2}[x,][^,x]{0,2}|[^,x]{0,2}[,][^,x]{0,2})[1-9][0-9]{1}[,x][1-9][0-9]{1}$'
-    # Explanation of the regular expression:
-    # ^ and $ are anchors that match the start and end of the string respectively.
-    # (?=[1-9][0-9]{1}) is a positive lookahead that checks if the string has at least one 2 digit number that starts with a digit from 1 to 9.
-    # (?=[^,x]{0,2}[x,][^,x]{0,2}|[^,x]{0,2}[,][^,x]{0,2}) is another positive lookahead that checks if the string has a comma or an "x" in the middle, surrounded by up to 2 non-comma/non-"x" characters on each side.
-    # [1-9][0-9]{1} matches a 2 digit number that starts with a digit from 1 to 9.
-    # [,x] matches either a comma or an "x".
-    # The final [1-9][0-9]{1} matches another 2 digit number that starts with a digit from 1 to 9.
-    if re.match(pattern, value):
-        values = value.split(value[2])
-        return GridSize(values[0], values[1]), None
-    else:
-        return None, f"Invalid value for argument: {argument_name}, expected format: {allowed_format} actual value: {value}"
+
 
 def main():
-    cli_arg_parser_config: ArgumentParserConfig = ArgumentParserConfig([
-        Subcommand("grid-size", "GS", grid_size_validator, False)
-    ])
-    cli_argument_parser: ArgumentParser = ArgumentParser(cli_arg_parser_config)
-
-    game_settings = cli_argument_parser.parse(sys.argv[1:])
+    game_settings = SubcommandParser([
+        GridSize()
+    ]).parse(sys.argv[1:])
 
     # grid_size = (30, 10)
     # game = Tetris(grid_size)
