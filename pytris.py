16595bec2a1d4e4813a818ccf6332acfc17b05e0
Kovacs Ervin <kov.ervin9@gmail.com>
2023-03-19 18:58:52 +0200

    tetromino dropping and shit but rethinking the whole universe

--- pytris.py
+++ pytris.py
@@ -6,116 +6,59 @@ import os
 import sys
 import re
 from enum import Enum
+import logging
+import asyncio
+import keyboard
+import queue
 
-T = TypeVar('T')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
 
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
 
-class Subcommand(ABC):
-    @abstractmethod
-    def validate():
-        pass
+file_handler = logging.FileHandler('logs.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
 
+logger.addHandler(file_handler)
+logger.addHandler(stdout_handler)
 
-class Position:
-    def __init__(self, row_index: int, col_index: int) -> None:
-        self.row_index = row_index
-        self.col_index = col_index
+class Movement(Enum):
+    LEFT = 1,
+    RIGHT = 2
 
+movements = queue.Queue()
 
-class GridSize(Subcommand):
-    def __init__(self) -> None:
-        self.name = 'grid_size'
-        self.shorthand = 'GS'
+async def read_input():
+    while True:
+        if keyboard.is_pressed('l'):
+            movements.put(Movement.RIGHT)
+        if keyboard.is_pressed('h'):
+            movements.put(Movement.LEFT)
+        await asyncio.sleep(0.1)
 
-    def validate(self, arg_value: str):
-        allowed_format = "<width: int><separator: @see allowed_separators><height: int>"
-        pattern = r'^(?=[1-9][0-9]{1})(?=[^,x]{0,2}[x,][^,x]{0,2}|[^,x]{0,2}[,][^,x]{0,2})[1-9][0-9]{1}[,x][1-9][0-9]{1}$'
-        if re.match(pattern, arg_value):
-            values = arg_value.split(arg_value[2])
-            return (values[0], values[1]), None
-        else:
-            return None, f"Invalid value for argument: {self.name}, expected format: {allowed_format} actual value: {arg_value}"
+async def run_tetris():
+    grid_size = (30, 10)
+    game = Tetris(grid_size)
+    await game.start()
 
-
-class SubcommandParser:
-    def __init__(self, settings: List[Subcommand]) -> None:
-        self.settings = settings
-
-    def parse(self, argv_str) -> GameSettings:
-        valid_results = []
-        pairs = self.parse_to_pairs(argv_str)
-
-        pair_by_validator = self.get_pairs_by_validator(pairs)
-
-        for entry in pair_by_validator:
-            print(entry)
-            if entry[0] is not None:
-                validation = entry[0].value_validator(
-                    entry[0].name, entry[1][1])
-                match validation:
-                    case (_, None):
-                        valid_results.append(validation)
-                    case _: pass
-        return valid_results
-
-    def parse_to_pairs(self, args: List[str]):
-        argument_pairs = []
-        if len(args) % 2 == 1:
-            args.append("_")
-        for first, second in zip(args[::2], args[1::2]):
-            argument_pairs.append((first, second.replace("_", "")))
-        return argument_pairs
-
-    def get_pairs_by_validator(self, pairs: List[tuple[str, None] | tuple[str, str]]):
-        result = []
-        for idx, pair in enumerate(pairs):
-            t = self.find_setting(pair, idx)
-            result.append((t[0], pair))
-        return result
-
-    def find_setting(self, pair: (tuple[str, None] | tuple[str, str]), index: int) -> (tuple[Subcommand, None] | tuple[None, str]):
-        if not pair[0].startswith("--"):
-            return None, f"Error parsing argument [{index}] [{pair[0]}]: Argument name must start with: '--'"
-
-        argument_name = pair[0][2:]
-        for s in self.settings:
-            if s.name == argument_name:
-                return s, None
-        return None, f"Invalid argument [{index}] [{pair[0]}]"
-# ARGUMENT PARSER END
-
-
-# Arguments:
-# grid_size
-
-
-def main():
-    game_settings = SubcommandParser([
-        GridSize()
-    ]).parse(sys.argv[1:])
-
-    # grid_size = (30, 10)
-    # game = Tetris(grid_size)
-    # game.start()
-
-
-# def parse_args(args: List[str]) -> GameSettings:
-#     return GameSettings()
+async def main():
+    listen_for_keypress = asyncio.create_task(read_input())
+    run_game = asyncio.create_task(run_tetris())
+    await asyncio.wait([listen_for_keypress, run_game], return_when=asyncio.FIRST_COMPLETED)
 
 
 class Tetris:
     # grid_size: tuple of 2 elements: width, height
     def __init__(self, grid_size):
-        # TODO:
-        # [ ] - validate grid size, it must be at least the size of the smallest tetrominoe
         self.grid_size = grid_size
         self.grid = [[0] * grid_size[0] for _ in range(grid_size[1])]
         self.in_game_tetrominoes = []
-        # NOTE: find another solution for keeping track of the tetrominoe type
-        self.in_game_tetrominoes.append(tetrominoe(
-            representation=TetrominoeType(1), starting_position=(0, 0)))
 
-        # absolute dog shit initialization for borders
+        # absolute dogshit initialization for borders
         top_row = [1 for _ in range(grid_size[0])]
         top_row[:0] = [3]
         top_row.append(4)
@@ -132,58 +75,102 @@ class Tetris:
             bottom_row
         )
 
-    def start(self):
-        # make this dynamic
-        while not self.game_over():
-            # validate game by checking if there is anything in the first row
-            self.render()
+    async def start(self):
+        # while not self.game_over():
+        iterations = 20
+        while iterations > 0:
+            position = (0, random.choice(range(1, self.grid_size[0])))
+            next_piece = self.generate_tetrominoe(position)
+            self.in_game_tetrominoes.append(next_piece)
+            await self.do_cycle()
+            iterations -= 1
 
-            # the tetrominoe for the current iteration is always the last one from the list
-            self.in_game_tetrominoes[-1].move_down()
-
-    def iterate(self):
-        # generating new tetrominoe
-        random_tetrominoe_index = random.choice(range(1, 4))
-        tetrominoe_starting_col_index = random.choice(
-            range(0, self.grid_size[0]))
-        starting_position = (0, tetrominoe_starting_col_index)
-        self.in_game_tetrominoes.append(tetrominoe(representation=TetrominoeType(
-            random_tetrominoe_index), starting_position=starting_position))
-
-    def do_cycle(self):
-        os.system("clear")
-        self.render()
-        time.sleep(1)
+    async def do_cycle(self):
+            can_move = True
+            while can_move:
+                os.system("clear")
+                self.render()
+                await asyncio.sleep(0.3)
+                tetrominoe = self.in_game_tetrominoes[-1]
+                while not movements.empty():
+                    match movements.get():
+                        case Movement.LEFT:
+                            tetrominoe.move_left()
+                        case Movement.RIGHT:
+                            tetrominoe.move_right()
+                        case _:
+                            pass
+                can_move = tetrominoe.position[0] + tetrominoe.height() < self.grid_size[1] and tetrominoe.move_down(self.in_game_tetrominoes[:-1])
 
     def render(self):
         # print top row
         for c in self.grid_borders[0]:
-            print(c, end='')
+            uc_char = self.get_unicode_char_by_number(c)
+            print(uc_char, end='')
         print('')
 
         # print grid content
         for row_index, row in enumerate(self.grid):
             # print left border for each row
-            print(self.grid_borders[1], end='')
+            border_char = self.grid_borders[1]
+            uc_char = self.get_unicode_char_by_number(border_char)
+            print(uc_char, end='')
 
             # print row
             for col_index, col in enumerate(row):
                 # loop through all in game tetrominoes
                 current_index = (row_index, col_index)
-                current_char = 'x' if self.is_any_tetrominoe_intersecting(
+                current_char = 99 if self.is_any_tetrominoe_intersecting(
                     current_index) else ' '
-                print(current_char, end='')
+                unicode = self.get_unicode_char_by_number(current_char)
+                print(unicode, end='')
 
             # print right border for each row
-            print(self.grid_borders[1], end='')
-            # print an empty line between rows
+            uc_char = self.get_unicode_char_by_number(border_char)
+            print(uc_char, end='')
             print('')
 
         # print bottom row
         for c in self.grid_borders[2]:
-            print(c, end='')
+            uc_char = self.get_unicode_char_by_number(c)
+            print(uc_char, end='')
         print('')
 
+    def generate_tetrominoe(self,position):
+        index = random.choice(range(1, 5))
+        match index:
+            case 1:
+                return VL(position)
+            case 2:
+                return HL(position)
+            case 3:
+                return L(position)
+            case 4:
+                return T(position)
+            case _:
+                return VL(position)
+
+    def get_unicode_char_by_number(self, number):
+        match number:
+            case 1:
+                return "\u2501"
+            case 2:
+                return "\u2503"
+            case 3:
+                return "\u250F"
+            case 4:
+                return "\u2513"
+            case 5:
+                return "\u2517"
+            case 6:
+                return "\u251B"
+            case 99:
+                return "\u2591"
+            case _:
+                return number
+
+
+    # TODO: randoml generate orientation for the piece
     def generate_random_starting_position_for_tetrominoe(self):
         return random.choice(range(0, self.grid_size[0]))
 
@@ -199,43 +186,123 @@ class Tetris:
         return False
 
 
-class tetrominoe:
-    def __init__(self, representation, starting_position):
-        self.representation = representation
-        self.position = starting_position
+class Tetrominoe(ABC):
+    @abstractmethod
+    def representation(self) -> List[tuple[int, int]]:
+        pass
 
-    def move_down(self):
+    @abstractmethod
+    def height(self) -> int:
+        pass
+
+    @abstractmethod
+    def width() -> int:
+        pass
+
+    def move_down(self, in_game_pieces):
         self.position = (self.position[0] + 1, self.position[1])
+        current = set(self.representation())
+        if any((set(piece.representation()) & current) for piece in in_game_pieces):
+            self.position = (self.position[0] - 1, self.position[1])
+            return False
+        return True 
 
-    # TODO: verify bounds, currently it can move out of the map
+    # FIXME: verify bounds, currently it can move out of the map
     def move_left(self):
         self.position = (self.position[0], self.position[1] - 1)
 
-    # TODO: verify bounds, currently it can move out of the map
+    # FIXME: verify bounds, currently it can move out of the map
     def move_right(self):
         self.position = (self.position[0], self.position[1] + 1)
 
     def intersects(self, coords):
-        # TODO: somehow verify if the position of the current tetrominoe intersect the coordinates passed
+        # FIXME: somehow verify if the position of the current tetrominoe intersect the coordinates passed
         # if any((c[0] == coords[0] and c[1] == coords[1]) for c in vertical_line(self.position)):
         #     print(vertical_line(self.position))
-        return any((c[0] == coords[0] and c[1] == coords[1]) for c in vertical_line(self.position))
+        return any((c[0] == coords[0] and c[1] == coords[1]) for c in self.representation())
 
 
-# coords: (row, col)
-def vertical_line(pos):
-    return [
-        (pos[0], pos[1]),
-        (pos[0] + 1, pos[1]),
-        (pos[0] + 2, pos[1])
-    ]
+class VL(Tetrominoe):
+    def __init__(self, starting_position):
+        self.position = starting_position
+    
+    def representation(self) -> List[tuple[int, int]]:
+        row = self.position[0]
+        col = self.position[1]
+        return [
+            (row,     col),
+            (row + 1, col),
+            (row + 2, col)
+        ]
+    
+    def height(self) -> int:
+        return 3
+
+    def width(self) -> int:
+        return 1
 
 
-class TetrominoeType(Enum):
-    VERTICAL_LINE = 1
-    HORIZONTAL_LINE = 2
-    BOX = 3
-    L_FORM = 4
+class HL(Tetrominoe):
+    def __init__(self, starting_position):
+        self.position = starting_position
+    
+    def representation(self) -> List[tuple[int, int]]:
+        row = self.position[0]
+        col = self.position[1]
+        return [
+            (row, col),
+            (row, col + 1),
+            (row, col + 2)
+        ]
+
+    def height(self) -> int:
+        return 1
+
+    def width(self) -> int:
+        return 3
 
 
-main()
+class L(Tetrominoe):
+    def __init__(self, starting_position):
+        self.position = starting_position
+    
+    def representation(self) -> List[tuple[int, int]]:
+        row = self.position[0]
+        col = self.position[1]
+        return [
+            (row, col),
+            (row + 1, col),
+            (row + 2, col),
+            (row + 2, col + 1),
+            (row + 2, col + 2)
+        ]
+
+    def height(self) -> int:
+        return 3
+
+    def width(self) -> int:
+        return 2
+
+
+class T(Tetrominoe):
+    def __init__(self, starting_position):
+        self.position = starting_position
+    
+    def representation(self) -> List[tuple[int, int]]:
+        row = self.position[0]
+        col = self.position[1]
+        return [
+            (row, col),
+            (row, col + 1),
+            (row, col + 2),
+            (row + 1, col + 1),
+            (row + 2, col + 1)
+        ]
+
+    def height(self) -> int:
+        return 3
+
+    def width(self) -> int:
+        return 3
+
+asyncio.run(main())
---
+++ todo.md
@@ -0,0 +1,6 @@
+# idea:
+# i could have an option object that has a subcommand property 
+# iterate over the options with the sys.argv ->
+# if the option has a subcommand then parse it with the validator
+# if succesfully parsed -> set it in the game
+
